d583d62f2f95ce92cf1dfca7ab544ced0f22029b
Federickg23 <federickg23@gmail.com>
2020-07-08 13:49:31 +0000

    Complete test with no mandatory attendees

--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -42,9 +42,13 @@ public final class FindMeetingQueryTest {
   private static final int TIME_0900AM = TimeRange.getTimeInMinutes(9, 0);
   private static final int TIME_0930AM = TimeRange.getTimeInMinutes(9, 30);
   private static final int TIME_1000AM = TimeRange.getTimeInMinutes(10, 0);
+  private static final int TIME_1030AM = TimeRange.getTimeInMinutes(10, 30);
   private static final int TIME_1100AM = TimeRange.getTimeInMinutes(11, 00);
+  private static final int TIME_1200PM = TimeRange.getTimeInMinutes(12, 00);
+  private static final int TIME_0100PM = TimeRange.getTimeInMinutes(13, 00);
 
   private static final int DURATION_30_MINUTES = 30;
+  private static final int DURATION_45_MINUTES = 45; 
   private static final int DURATION_60_MINUTES = 60;
   private static final int DURATION_90_MINUTES = 90;
   private static final int DURATION_1_HOUR = 60;
@@ -176,9 +180,6 @@ public final class FindMeetingQueryTest {
 
   @Test
   public void overlappingEvents() {
-    // Have an event for each person, but have their events overlap. We should only see two options.
-    //
-    // Events  :       |--A--|
     //                     |--B--|
     // Day     : |---------------------|
     // Options : |--1--|         |--2--|
@@ -277,6 +278,33 @@ public final class FindMeetingQueryTest {
   }
 
   @Test
+  public void noRoomWithOptional() {
+    // Have one person, but make it so that there is not enough room at one point in the day to
+    // have the meeting with all attendees, so the optional attentee should be ignored.
+    //
+    // Events   : |--A--|     |----A----|
+    // Opttional:         |B|
+    // Day      : |---------------------|
+    // Options  :       |-----|
+
+    Collection<Event> events = Arrays.asList(
+        new Event("Event 1", TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0830AM, false),
+            Arrays.asList(PERSON_A)),
+        new Event("Event 2", TimeRange.fromStartEnd(TIME_0900AM, TimeRange.END_OF_DAY, true),
+            Arrays.asList(PERSON_A)),
+        new Event("Event 3", TimeRange.fromStartEnd(TIME_0900AM, DURATION_45_MINUTES, true),
+            Arrays.asList(PERSON_B)));
+
+    MeetingRequest request = new MeetingRequest(Arrays.asList(PERSON_A), DURATION_30_MINUTES);
+    request.addOptionalAttendee(PERSON_B);
+
+    Collection<TimeRange> actual = query.query(events, request);
+    Collection<TimeRange> expected =
+        Arrays.asList(TimeRange.fromStartDuration(TIME_0830AM, DURATION_30_MINUTES));
+    Assert.assertEquals(expected, actual);
+  }
+
+  @Test
   public void ignoresPeopleNotAttending() {
     // Add an event, but make the only attendee someone different from the person looking to book
     // a meeting. This event should not affect the booking.
@@ -322,6 +350,31 @@ public final class FindMeetingQueryTest {
     Collection<TimeRange> expected = Arrays.asList();
 
     Assert.assertEquals(expected, actual);
+  }
+
+  @Test 
+  public void noMandatoryAttendees(){
+     // Two optional attendees with gaps in their schedule. 
+     // 
+     Collection<Event> events = Arrays.asList(
+        new Event("Event 1", TimeRange.fromStartDuration(TIME_0930AM, DURATION_60_MINUTES), 
+            Arrays.asList(PERSON_A)), 
+        new Event("Event 2", TimeRange.fromStartDuration(TIME_1200PM, DURATION_60_MINUTES), 
+            Arrays.asList(PERSON_B)));
+        
+    MeetingRequest request = new MeetingRequest(NO_ATTENDEES, DURATION_30_MINUTES); 
+        request.addOptionalAttendee(PERSON_A);
+        request.addOptionalAttendee(PERSON_B);
+    Collection<TimeRange> actual = query.query(events, request);
+    Collection<TimeRange> expected =
+        Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0930AM, false), 
+                      TimeRange.fromStartEnd(TIME_1030AM, TIME_1200PM, false),
+                      TimeRange.fromStartEnd(TIME_0100PM, TimeRange.END_OF_DAY,true));
+
+    Assert.assertEquals(expected, actual);
+
+
+
   }
 }
 
